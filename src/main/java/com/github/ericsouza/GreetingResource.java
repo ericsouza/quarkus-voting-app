db4f2349ca4162429983d1d657e8f8640b5b097c
Eric Souza <ericsouza0801@gmail.com>
2021-02-27 11:28:52 -0300

    Add index hello template and removed qute default files

--- src/main/java/com/github/ericsouza/GreetingResource.java
+++ src/main/java/com/github/ericsouza/GreetingResource.java
@@ -1,16 +1,24 @@
 package com.github.ericsouza;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.api.ResourcePath;
+
+@ApplicationScoped
+@Path("/")
 public class GreetingResource {
 
+    @ResourcePath("index.html")
+    Template indexTpl;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public TemplateInstance hello() {
+        return indexTpl.data("myHello", "hello world!!!!");
     }
 }
